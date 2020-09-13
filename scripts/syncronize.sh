1bd8eb4b0097eb1605ed5c966a4f52d5d769aee9
hideki1217 <okumurahidekikangeki@gmail.com>
2020-09-13 08:46:28 +0000

    taskを変更追加

--- scripts/syncronize.sh
+++ scripts/syncronize.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#テンプレートを同期
+#コンテスト用にテンプレートをコピー
 dir_name="$(cd $(dirname $0); pwd)"
-dir_name=${dir_name//scripts}
+dir_name=${dir_name///scripts}
 
-cp $dir_name/field/practice/template.cc $dir_name/field/template/A.cc
-cp $dir_name/field/practice/template.cc $dir_name/field/template/B.cc
-cp $dir_name/field/practice/template.cc $dir_name/field/template/C.cc
-cp $dir_name/field/practice/template.cc $dir_name/field/template/D.cc
-cp $dir_name/field/practice/template.cc $dir_name/field/template/E.cc
-cp $dir_name/field/practice/template.cc $dir_name/field/template/F.cc
+mkdir $dir_name/field/contests/${1}
+
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/A.cc
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/B.cc
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/C.cc
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/D.cc
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/E.cc
+cp $dir_name/field/practice/template.cc $dir_name/field/contests/${1}/F.cc
