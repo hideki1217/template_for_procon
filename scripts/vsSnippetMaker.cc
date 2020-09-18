e60f772b3834e6a27e023eb68aae7a44702a27df
hideki1217 <okumurahidekikangeki@gmail.com>
2020-09-18 14:00:40 +0000

    テストコード生成

--- scripts/vsSnippetMaker.cc
+++ scripts/vsSnippetMaker.cc
@@ -3,6 +3,8 @@
 #include<string>
 #include<fstream>
 #include<regex>
+#include<stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -11,12 +13,12 @@ using namespace std;
 int intend=4;
 string ifname,ofname;
 string body,name,prefix,scope,description;
-vector<string> reg;
+vector<string> reg_first,reg_second;
 
 int makeSnipett(ifstream &in,ofstream &out){
     out << "{" << endl;
 
-    for(auto s:reg){
+    for(auto s:reg_first){
         out << s <<endl;
     }
 
@@ -59,7 +61,9 @@ int makeSnipett(ifstream &in,ofstream &out){
     out << "\t\t]," <<endl;
     out <<"\t\t\"description\":\""+description+"\"" <<endl;
     out << "\t}," <<endl;
-
+    for(auto s:reg_second){
+        out << s <<endl;
+    }
     out << "}" <<endl;
     return 0;
 }
@@ -68,17 +72,19 @@ void Main(){
     string ans;
     do{
         cout << "# What is snipett name ?" <<endl;
-        cin >> name;
+        cout<<"::";cin >> name;
         cout << "# What language is used in this ? Exmple:cpp,python,javascript,etc.." <<endl;
-        cin >> scope;
+        cout<<"::";cin >> scope;
         cout << "# What is prefix ? " <<endl;
-        cin >> prefix;
+        cout<<"::";cin >> prefix;
         cout << "# Write any description ?? Y/n " <<endl;
         cin >> ans;
         if(ans[0]=='Y'||ans[0]=='y'){
             cout << "## write your description below" <<endl;
-            cin >> description;
+            cin.ignore();
+            cout<<"::";getline(cin,description);
         } 
+        sleep(1);
         cout << "==============" <<endl;
         cout << "  name:"+name+"\n  scope:"+scope+"\n  prefix:"+prefix +"\n description:\""+ description +"\""<<endl;
         cout << "==============" <<endl;
@@ -86,23 +92,43 @@ void Main(){
         cin>>ans;
     }while(ans[0]=='n'||ans[0]=='N');
 
-    cout << "making snipett ..." <<endl;
+    /*printf("making snipett  ");
+    sleep(1);
+    printf("making snipett.  ");
+    sleep(1);
+    printf("making snipett.. ");*/
+    sleep(1);
+    printf("making snipett...");
 
     ifstream st(ofname);
     if(!st){
-        cerr<<"I failed to open output file"<<endl;
-        cerr<<"maybe there is no such file"<<endl;
-        cerr<<"so create file"<<endl;
+        cout<<"I failed to open output file"<<endl;
+        cout<<"maybe there is no such file"<<endl;
+        cout<<"so create file"<<endl;
     }
     string line;
     smatch sm;
     while(getline(st,line)){
-        if(regex_match(line,sm,regex(".*("+name+"|"+prefix+").*"))){
-            cout << "***same name or prefix exist***" <<endl;
-            return;
+        if(regex_match(line,sm,regex("\t\""+name+"\":\\{"))){
+            cout << "***same name exist***" <<endl;
+            sleep(1);
+            cout << "# Do you want to OVERWRITE ?? (Y/n)" <<endl;
+            cin >> ans;
+            if(ans[0] != 'Y'&& ans[0] != 'y'){cout << "Stop.." <<endl;return;}
+            while(getline(st,line)){
+                if(regex_match(line,sm,regex(R"(\t\},.)")))break;
+            }
+            break;
+        }else if(regex_match(line,sm,regex("\t\t\"prefix\":.*\""+prefix+"\".*"))){
+            cout << "***same prefix exist***" <<endl;
+            cout << "Stop..." <<endl; return;
         }
         if(regex_search(line,sm,regex(R"(^\})"))||regex_search(line,sm,regex(R"(^\{)")))continue;
-        reg.push_back(line);
+        reg_first.push_back(line);
+    }
+    while(getline(st,line)){
+        if(regex_search(line,sm,regex(R"(^\})"))||regex_search(line,sm,regex(R"(^\{)")))continue;
+        reg_second.push_back(line);
     }
     st.close();
 
